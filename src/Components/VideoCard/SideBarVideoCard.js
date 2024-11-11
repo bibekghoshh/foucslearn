3a36c34b0c5a4a4dd3049c9b0c1ff7f983686fc5
bibekghoshh <bibek7478@gmail.com>
2024-11-11 13:21:18 +0530

    rearrange the sidebar

--- src/Components/VideoCard/SideBarVideoCard.js
+++ src/Components/VideoCard/SideBarVideoCard.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { MdOutlineOndemandVideo } from "react-icons/md";
+import { MdCheckBoxOutlineBlank, MdOutlineOndemandVideo } from "react-icons/md";
 
 const SideBarVideoCard = ({ items, videolength }) => {
-
   return (
-    <div className="pl-4 border-b-[0.5px] shadow-sm py-2 px-2 cursor-pointer hover:bg-slate-300">
-      <div className="flex w-[400px] cursor-pointer hover:bg-slate-300  text-sm">
-        <div>{items.snippet.position}.</div>
-        <div className="ml-2">{items.snippet.title}</div>
-      </div>
-      <div className="flex items-center gap-1 mt-[6px] text-slate-500 text-sm">
-        <div><MdOutlineOndemandVideo /></div>
-        <div className="text-sm">{videolength}</div>
+    <div className="pl-4 w-[400px] border-b-[0.5px] shadow-sm py-2 px-2 cursor-pointer hover:bg-slate-300">
+      <div className="flex w-[400px] gap-3">
+        <div className="mt-[2px] text-[20px]">
+          <MdCheckBoxOutlineBlank />
+        </div>
+        <div>
+          <div className="flex  text-sm">
+            <div>{items.snippet.position}.</div>
+            <div className="ml-1">{items.snippet.title}</div>
+          </div>
+          <div className="flex items-center gap-1 mt-[6px] text-slate-500 ">
+            <div className="text-[16px]">
+              <MdOutlineOndemandVideo />
+            </div>
+            <div className="text-xs">{videolength}</div>
+          </div>
+        </div>
       </div>
     </div>
   );
